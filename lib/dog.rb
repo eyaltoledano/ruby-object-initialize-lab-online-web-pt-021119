11145c972e39f129b17454d32f75ef4024479bdc
Eyal Toledano <eyaltoledano@me.com>
2019-03-02 23:00:16 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+  def initialize(name, breed)
+    @name = name
+    if breed == nil
+      breed = "Mutt"
+    else @breed = breed
+    end
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+end
